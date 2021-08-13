bafaa361650e1300e3c0e3b1b76e7021efccdf69
Mykyta Babak <nykyta.babak@gmail.com>
2021-08-13 12:23:20 +0300

    added some tests

--- ProjectTest/ProjectTest/UnitTest1.cs
+++ ProjectTest/ProjectTest/UnitTest1.cs
@@ -1,18 +1,47 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace ProjectTest
 {
     public class Tests
     {
+        private IWebDriver driver;
+
+        private readonly By _signInButton = By.XPath("//a[text()='Войти']");
+        private readonly By _signInContinueButton = By.XPath("//span[text()='Далее']");
+
+        private readonly By _signInEmailLabel = By.XPath("//input[@id='identifierId']");
+
+
+        private const string _email = "zaraznaya.pochta@gmail.com";
+
         [SetUp]
         public void Setup()
         {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://google.com");
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var signIn = driver.FindElement(_signInButton);
+            signIn.Click();
+
+
+            var email = driver.FindElement(_signInEmailLabel);
+            email.SendKeys(_email);
+
+            var signInContinue = driver.FindElement(_signInContinueButton);
+            signInContinue.Click();
+            //Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
         }
     }
 }
