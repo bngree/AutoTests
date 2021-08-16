c329f79edcea5b820b2f477b299a90f70b3e51b5
Mykyta Babak <nykyta.babak@gmail.com>
2021-08-16 12:26:52 +0300

    finished test

--- ProjectTest/ProjectTest/UnitTest1.cs
+++ ProjectTest/ProjectTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
+using System.Threading;
 
 namespace ProjectTest
 {
@@ -9,11 +10,13 @@ namespace ProjectTest
 
         private readonly By _signInButton = By.XPath("//a[text()='Войти']");
         private readonly By _signInContinueButton = By.XPath("//span[text()='Далее']");
-
         private readonly By _signInEmailLabel = By.XPath("//input[@id='identifierId']");
+        private readonly By _errorMessageSpan = By.XPath("//h1[@id='headingText']/span");
 
 
-        private const string _email = "test@gmail.com";
+        private const string _email = "zaraznaya.pochta@gmail.com";
+        private const string expectedErrorText = "Не удалось войти в аккаунт";
+
 
         [SetUp]
         public void Setup()
@@ -24,20 +27,25 @@ namespace ProjectTest
         }
 
         [Test]
-        public void Test1()
+        public void TryToLogin()
         {
             var signIn = driver.FindElement(_signInButton);
             signIn.Click();
 
 
-
+            Thread.Sleep(400);
             var email = driver.FindElement(_signInEmailLabel);
             email.SendKeys(_email);
 
+            Thread.Sleep(400);
             var signInContinue = driver.FindElement(_signInContinueButton);
             signInContinue.Click();
 
+            Thread.Sleep(600);
+            var actualErrorText = driver.FindElement(_errorMessageSpan).Text;
+            Assert.AreEqual(expectedErrorText, actualErrorText, "Text is incorrect");
 
+            
 
             //Assert.Pass();
         }
@@ -51,7 +59,8 @@ namespace ProjectTest
         [TearDown]
         public void TearDown()
         {
-
+            Thread.Sleep(600);
+            driver.Quit();
         }
     }
 }
